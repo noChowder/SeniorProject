d9d0f8183cea73d6ec2897e2cb5bca2b78ea26b1
Bryan K Chau <bryankchau@gmail.com>
2022-12-05 10:45:51 -0800

    created test vs different subject eyes

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ def compare(test_des, descriptors):
         for e in matches:
             if (e.distance < 37):
                 count += 1
-        print("\nThere were " + str(count) + " feature pairs in this test.")
+        # print("\nThere were " + str(count) + " feature pairs in this test.")
         if (count > 10):
-            print("\nCorrectly matched with sample eye number:\t" + str(s+1))
+            # print("\nCorrectly matched with sample eye number:\t" + str(s+1))
+            print("\nEyes matched.")
         else:
             print("\033[31m" + "\nNot a match." + "\033[0m")
         print("__________________________________________________\n\n")
@@ -35,9 +36,26 @@ def test_pcll():
         descriptors.append(id.des)
     compare(descriptors[5-1], descriptors)
 
+def test_sitil_pscll():
+    descriptors = []
+    for i in range(1, 6):
+        dir = "./MMU-Iris-Database/29/left/pcll" + str(i) + ".bmp"
+        id = iris_detection(dir)
+        id.detect()
+        descriptors.append(id.des)
+    for i in range(1, 6):
+        dir = "./MMU-Iris-Database/34/left/sitil" + str(i) + ".bmp"
+        id = iris_detection(dir)
+        id.detect()
+        descriptors.append(id.des)
+    for i in range(5):
+        print("\nTesting sitil eye:\t" + str(i+1) + "\tagainst pcll:\t6-10\n")
+        compare(descriptors[i], descriptors[5:10])
+
 def main():
     # test_aeval()
-    test_pcll()
+    # test_pcll()
+    test_sitil_pscll()
     pass
 
 if __name__ == "__main__":
